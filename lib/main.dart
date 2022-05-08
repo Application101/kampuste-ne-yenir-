1281b511eaaf79c4790f644bee2bd3f5ec48e056
Beyzanur Yeşilyurt <67544104+beyzayyt@users.noreply.github.com>
2022-05-08 22:04:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,19 @@
 import 'package:flutter/material.dart';
-import 'campus_page_list.dart';
-
+import 'home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CampusApp(),
-    );
-  }
-}
-
-class CampusApp extends StatefulWidget {
-  const CampusApp({ Key? key }) : super(key: key);
-
-  @override
-  State<CampusApp> createState() => _CampusAppState();
-}
-
-class _CampusAppState extends State<CampusApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text('Kampüste \nNe \nYenir?',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600, fontSize: 50),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  FloatingActionButton(
-                    onPressed: (){
-                      //buraya navigation kodu gelecek kütükhaneye kaydetmek gerektiği için şu an yazmadım
-                    },
-                    child: Container(child: const Text('Giriş Yap'),
-                      color: Colors.white,
-                      padding: const EdgeInsets.all(20.0),
-
-                    ),
-                  ),
-                  FloatingActionButton(
-                    onPressed: (){
-                      //buraya da aynı şekilde
-                    },
-                    child: Container(child: const Text('Üye Olmadan \nDevam Et'),
-                      color: Colors.white,
-                      padding: const EdgeInsets.all(20.0),
-
-                    ),
-                  ),
-                ],
-              ),
-            )],)
-
-
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home:  CampusApp(),
     );
   }
 }
