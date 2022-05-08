5c82bff3834b5728de3cc04e065fb95a4802c377
beyzayesilyurtt <beyzanur.yesilyurt@gmail.com>
2022-05-08 20:17:39 +0300

    Added initial pages

---
+++ lib/campus_page_list.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class CampusPage extends StatefulWidget {
+  const CampusPage({Key? key}) : super(key: key);
+
+  @override
+  State<CampusPage> createState() => _CampusPageState();
+}
+
+class _CampusPageState extends State<CampusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('Campus Page'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Center(child: Text('Restaurant 1')),
+            padding: EdgeInsets.all(8.0),
+            color: Colors.deepOrange,
+          ),
+          Container(
+            child: Center(child: Text('Restaurant 2')),
+            padding: EdgeInsets.all(8.0),
+            color: Colors.deepOrange,
+          ),
+          Container(
+            child: Center(child: Text('Restaurant 3')),
+            padding: EdgeInsets.all(8.0),
+            color: Colors.deepOrange,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Icon(Icons.question_mark),
+              Container(
+                child: FloatingActionButton(onPressed: () {}),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Icon(Icons.question_mark),
+              Container(
+                child: FloatingActionButton(onPressed: () {}),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextField(
+            style: TextStyle(color: Colors.deepOrange),
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromARGB(0, 203, 203, 223),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/entry.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CampusApp extends StatefulWidget {
+  const CampusApp({ Key? key }) : super(key: key);
+
+  @override
+  State<CampusApp> createState() => _CampusAppState();
+}
+
+class _CampusAppState extends State<CampusApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.white,
+            height: 100,
+            width: 100,
+
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            width: 100,
+          ),
+        ],),
+
+    );
+  }
+}
