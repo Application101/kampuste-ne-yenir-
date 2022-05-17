063120dfb0b32f8ddc84cf4640fa3ac2c13d851a
beyzayesilyurtt <beyzanur.yesilyurt@gmail.com>
2022-05-17 17:03:04 +0300

    Changed json file.

--- lib/model/university_model.dart
+++ lib/model/university_model.dart
@@ -7,26 +7,22 @@ String userToJson(List<University> data) => json.encode(List<dynamic>.from(data.
 class University {
   University({
     required this.index,
-    required this.about,
     required this.restaurants,
     required this.name,
   });
 
   int index;
-  String about;
   List<String> restaurants;
   String name;
 
   factory University.fromJson(Map<String, dynamic> json) => University(
     index: json["index"],
-    about: json["about"],
     restaurants: List<String>.from(json["restaurants"].map((x) => x)),
     name: json["name"]
   );
 
   Map<String, dynamic> toJson() => {
     "index": index,
-    "about": about,
     "restaurants": List<dynamic>.from(restaurants.map((x) => x)),
     "name": name
   };
