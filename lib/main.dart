3fb4e85f5da903bd70dd378f2602da32e1ac333c
faTospsn <99354010+faTospsn@users.noreply.github.com>
2022-05-22 20:13:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'login_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CampusApp(),
+      home: LoginPage(),
     );
   }
 }
